025df87de3b4f21acd16c680c1781b94d7ca456d
unknown <quftpsk@naver.com>
2019-11-07 16:16:56 +0900

    'try to load data file'

--- transformer.py
+++ transformer.py
@@ -1,2 +1,20 @@
-print("Hello world")
-print("Tell Your World")
+import pandas as pd
+import numpy as np
+
+class data_file:
+    def __init__(self,file_name,form):
+        self.file_name=file_name
+        self.form=form
+
+        file=read_csv(file_name)
+        #f=open(file_name,'r')
+        #lines=f.readlines()
+        #while True:
+        #    line=f.readline()
+        #    if not line: break
+        #    print(line)
+        #f.close()
+        #for i in range(1,11):
+        #    data="%d번째 줄입니다.\n"%i
+        #    f.write(data)
+        #f.close()
