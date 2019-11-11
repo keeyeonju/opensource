9838cf9422bc0cdd48bf0bce69b4dc734365975e
unknown <quftpsk@naver.com>
2019-11-11 17:41:56 +0900

    'add open csv file'

--- transformer.py
+++ transformer.py
@@ -18,3 +18,44 @@ class data_file:
         #    data="%d번째 줄입니다.\n"%i
         #    f.write(data)
         #f.close()
+
+
+#csv파일
+data_csv=pd.read_csv("diabetes.csv")
+data_csv
+
+#pdf파일
+#pdf파일 불러와서 text파일로 읽어들이기
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+
+def convert_pdf_to_txt(file_name):
+    #pdf 리소스 매니저 객체 생성
+    rsrcmgr=PDFResourceManager()
+    #문자열 데이터를 파일처럼 처리하는 stringio->pdf 파일 내용이 여기 담김
+    retstr=StringIO()
+    codec='utf-8'
+    laparams=LAParams()
+    device=TextConverter(rsrcmgr,retstr,codec=codec,laparams=laparams)
+    fp=open(file_name,'rb')
+    interpreter=PDFPageInterpreter(rsrcmgr,device)
+    password=""
+    maxpages=0
+    caching=True
+    pagenos=set()
+    
+    for page in PDFPage.get_pages(fp,pagenos,maxpages-maxpages,password=password,caching=caching,check_extractable=True):
+        interpreter.process_page(page)
+    #text에 결과가 담김
+    text=retstr.getvalue()
+    
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+v=convert_pdf_to_txt('Grade_컴퓨터구조.pdf')
+print(v)
