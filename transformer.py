9699842e2a65ec62b1b27fa6f4d94c50fa3d9dab
keeyeonju <51349332+keeyeonju@users.noreply.github.com>
2019-11-12 15:34:32 +0900

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
 
 
 from pydub import AudioSegment
 from pydub.playback import play
 
+
+##audio class##
+
 class audio:
+    #audio 재생 함수
     def load_audio(name):
         if name[-3:]=="wav":
             sound = AudioSegment.from_file(name, format="wav")
@@ -17,13 +20,8 @@ class audio:
         play(sound)
 
 
-# In[ ]:
 
 
-
-
-
-# In[ ]:
 
 
 
