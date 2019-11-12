d064dfe0624074804cd52c83322ceffc5d7971e3
unknown <quftpsk@naver.com>
2019-11-12 16:38:35 +0900

    'changing class type'

--- transformer.py
+++ transformer.py
@@ -1,23 +1,14 @@
 import pandas as pd
 import numpy as np
 
-class data_file:
-    def __init__(self,file_name,form):
-        self.file_name=file_name
-        self.form=form
-
-        file=read_csv(file_name)
-        #f=open(file_name,'r')
-        #lines=f.readlines()
-        #while True:
-        #    line=f.readline()
-        #    if not line: break
-        #    print(line)
-        #f.close()
-        #for i in range(1,11):
-        #    data="%d번째 줄입니다.\n"%i
-        #    f.write(data)
-        #f.close()
+class data:
+    def open_data(file_name,xml_n):
+        if file_name[-3:]=='csv':
+            data
+        elif file_name[-4:]='json':
+            data
+        elif file_name[-3:]='xml'
+            data
 
 
 #---CSV 파일---
