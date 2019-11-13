678e2dc5ff2a2481a83af83a303d819c331f61da
unknown <quftpsk@naver.com>
2019-11-14 00:08:24 +0900

    'add read pdf_file'

--- transformer.py
+++ transformer.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
+
 from xml.etree.ElementTree import Element, SubElement, ElementTree, parse
+
+from tabula import read_pdf #pip install tabula-py
+import PyPDF2 #pip install PyPDF2
 
 class data:
     def open_data(name,xml_node1,xml_node2):
@@ -26,4 +30,43 @@ class data:
             xml_data=pd.DataFrame(rows,columns=[xml_node2])
             return xml_data
 
+        elif name[-3:]=='pdf':
+            reader = PyPDF2.PdfFileReader(open(name, mode='rb')) 
+            m = reader.getNumPages() 
+            #print(reader) 
+            print(m) 
+            for i in range(m): 
+                n = i+1 
+
+                if n==1: 
+                    df = read_pdf(name, pandas_options={'header': None, 'error_bad_lines': False}, pages=n) 
+                    index = np.where(df[0].isnull())[0] 
+                    sect = df.iloc[index[0]:index[-1]] 
+                    s = [] 
+                    headers = [] 
+                    for col in sect: 
+                        colnames = sect[col].dropna().values.flatten() 
+                        s.insert(len(s), colnames)
+                        pic = [' '.join(s[col])] 
+                        for i in pic: 
+                               headers.append(i) 
+                    print(df) 
+                    df.drop(sect, inplace=True) 
+                    df.columns = headers 
+                    new_df = pd.DataFrame(columns=headers) 
+                    new_df = pd.concat([new_df, df], axis=0, ignore_index=True) 
+
+                else: 
+                    df_2 = read_pdf(name, pandas_options={'header': None, 'error_bad_lines': False, 'encoding': "ISO-8859-1"}, pages=n) 
+                    df_2.drop(sect, inplace=True) 
+                    df_2.columns = headers 
+                    new_df = pd.concat([new_df, df_2], axis=0, ignore_index=True) 
+            
+            new_df.columns = headers 
+            print(new_df) 
+            new_df.to_csv(name, index=False)
+
+
+            
     #def to_pdf():
+        
