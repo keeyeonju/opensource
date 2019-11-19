01a954788c0f699529adcca5deb4404a11e638db
keeyeonju <keeyeonju@gmail.com>
2019-11-19 22:33:29 +0900

    파일 슬라이스추가

--- transformer.py
+++ transformer.py
@@ -10,10 +10,21 @@ from pydub.playback import play
 ##audio class##
 
 class audio:
-    def load_audio(name):
+    def load_audio(name="",start_sec=0,end_sec=0):
         if name[-3:]=="wav":
             sound = AudioSegment.from_file(name, format="wav")
-            play(sound)
+            if (start_sec!=0):
+                a=1000*start_sec
+                play(sound[a:])
+            
+            elif(end_sec==0):
+                play(sound)
+                
+            else:
+                a=1000*start_sec
+                b=1000*end_sec
+                play(sound[a:b])
+            
             return sound
         if name[-3:]=="mp3":
             sound = AudioSegment.from_file(name, format="mp3")
@@ -25,9 +36,12 @@ class audio:
     def save_audio(name,change_name):
         sound = AudioSegment.from_file(name, format=name[-3:])
         sound.export(change_name, format=change_name[-3:])
+
         
-#song=audio.load_audio("sample.wav")
+#song=audio.load_audio("sample.wav",0,3)
 #audio.save_audio("sample.wav","sample6.mp3")
+#last_5_seconds = song[-1000:]
+#play(last_5_seconds)
 
 
 
