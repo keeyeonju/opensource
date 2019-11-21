c5193060f971832482c6f94c0ea1c33df7051449
dudwls016 <dudwls016@gmail.com>
2019-11-21 16:04:59 +0900

    컬러영상및기본저장

--- transformer.py
+++ transformer.py
@@ -1,25 +1,27 @@
-import numpy as np
 import cv2
-
-file = 'test.mp4'
-cap = cv2.VideoCapture(file)
-
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+ 
+cap = cv2.VideoCapture("test.mp4"); 
+ 
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print("size: {0} x {1}".format(width, height))
+ 
+# 영상 저장을 위한 VideoWriter 인스턴스 생성
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+writer = cv2.VideoWriter('test.avi', fourcc, 24, (int(width), int(height)))
+ 
+while cap.isOpened():
+    success, frame = cap.read()
+    if success:
+        writer.write(frame)  # 프레임 저장
+        cv2.imshow('Video Window', frame)
+ 
+        # q 를 누르면 종료
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
+    else:
         break
-
-# When everything done, release the capture
+ 
 cap.release()
+writer.release()  # 저장 종료
 cv2.destroyAllWindows()
