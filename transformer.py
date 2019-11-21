2b759803056b483e6308501e2107f597bb5ae7c7
dudwls016 <dudwls016@gmail.com>
2019-11-21 15:39:38 +0900

    video open

--- transformer.py
+++ transformer.py
@@ -1,36 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
+import numpy as np
+import cv2
 
+file = 'test.mp4'
+cap = cv2.VideoCapture(file)
 
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
-from pydub import AudioSegment
-from pydub.playback import play
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-##audio class##
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-class audio:
-    #audio 재생 함수
-    def load_audio(name):
-        if name[-3:]=="wav":
-            sound = AudioSegment.from_file(name, format="wav")
-        if name[-3:]=="mp3":
-            sound = AudioSegment.from_file(name, format="mp3")
-
-        play(sound)
-
-class video:
-    #video 재생 함수
-     def load_video(name):
-         if name[-3:]=="avi":
-             video = videosegment.from_file(name, format="avi")
-         if name[-3:]=="wmv":
-             video = videosegment.from_file(name, format="wmv")
-
-        play(sound)
-
-
-
-
-
-
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
