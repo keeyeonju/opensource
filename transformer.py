1922f71af659223e063ca4632047d2952d05254d
dudwls016 <dudwls016@gmail.com>
2019-11-21 17:19:10 +0900

    3가지 변환추가

--- transformer.py
+++ transformer.py
@@ -1,26 +1,47 @@
 import cv2
- 
-cap = cv2.VideoCapture("test.mp4"); 
- 
+import os
+
+string = input().split()
+fileName = os.path.splitext(string[0])[0]
+ext = string[1]
+print(fileName,ext)
+cap = cv2.VideoCapture(string[0])
+
 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-print("size: {0} x {1}".format(width, height))
+print("size: {0} x {1}".format(320, 240))
  
 # 영상 저장을 위한 VideoWriter 인스턴스 생성
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-writer = cv2.VideoWriter('test.avi', fourcc, 24, (int(width), int(height)))
- 
+
+
 while cap.isOpened():
     success, frame = cap.read()
     if success:
-        writer.write(frame)  # 프레임 저장
-        cv2.imshow('Video Window', frame)
+        cv2.imshow('Video Window', frame) # 보여주기 part
  
-        # q 를 누르면 종료
         if cv2.waitKey(1) & 0xFF == ord('q'): 
             break
     else:
         break
+
+if(ext == "avi"):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    writer = cv2.VideoWriter(fileName+".avi", fourcc, 24, (int(width), int(height)))
+elif(ext == "mp4"):
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    writer = cv2.VideoWriter(fileName+".mp4", fourcc, 24, (int(width), int(height)))
+elif(ext == "wmv"):
+    fourcc = cv2.VideoWriter_fourcc(*'WMV2')
+    writer = cv2.VideoWriter(fileName+".wmv", fourcc, 24, (int(width), int(height)))
+else:
+    print("FILE EXT ERROR")
+while cap.isOpened():
+    success, frame = cap.read()
+    if success:
+        writer.write(frame)  # 프레임 저장
+    else:
+        break
+
  
 cap.release()
 writer.release()  # 저장 종료
