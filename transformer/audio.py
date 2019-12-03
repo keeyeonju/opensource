f660e1ea0ac8bf977b320509cb62dab60269d225
keeyeonju <keeyeonju@gmail.com>
2019-12-03 18:36:13 +0900

    pdf클래스 삭제

--- transformer/audio.py
+++ transformer/audio.py
@@ -54,43 +54,4 @@ class audio:
         sound.export(change_name, format=change_name[-3:])
 
 
-
-##Pdf class##
-
-class pdf:
-    def pdfSlice(file_name,first_page=0,last_page=0):
-
-        pdf=PdfFileReader(open(file_name,'rb'))
-
-        numberPages=pdf.getNumPages()
-        if(last_page==0):
-            pdf_writer=PdfFileWriter()
-            pdf_writer.addPage(pdf.getPage(first_page-1))
-
-            output_filename='{}_{}.pdf'.format(file_name[:-4],first_page)
-
-            with open (output_filename,'wb')as f:
-                pdf_writer.write(f)
-            return pdf
-
-        if(last_page>numberPages):
-            print("페이지 범위를 초과했습니다.")
-            return 0
-
-        pdf_writer=PdfFileWriter()
-        
-        for page in range(first_page-1,last_page):
-
-            #pdf_writer=PdfFileWriter()
-            pdf_writer.addPage(pdf.getPage(page))
-
-            #output_filename='{}_{}.pdf'.format(file_name[:-4],page+1)
-        output_filename='{}_{}_{}.pdf'.format(file_name[:-4],first_page,last_page)
-
-        with open (output_filename,'wb')as f:
-                pdf_writer.write(f)
-
-        return pdf
-
-#pdf.pdfSlice("frozen.pdf",1,3) #여러페이지 저장(1페이지부터 3페이까지 저장)
-#pdf.pdfSlice("frozen.pdf",3) #한페이지만 저장        
+  
